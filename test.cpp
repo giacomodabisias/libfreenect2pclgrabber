e0cbe3c071e5d5543d5b3364ffb220a8d109084b
Giacomo Dabisias <giacomo.dabisias@gmail.com>
2014-12-04 16:21:51 +0100

    all fixed

--- test.cpp
+++ test.cpp
@@ -1,7 +1,24 @@
+//Copyright 2014 Giacomo Dabisias
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//This is preliminary software and/or hardware and APIs are preliminary and subject to change.
 #include "Kinect2Grabber.hpp"
 
 int main(int argc, char *argv[])
 {
+  //The first constructor calibrates the cameras using images inside the specified folders.
+  //The parameters are: rgb image folder, depth image folder, number of images, size of the checkboard, size of the check board squares in m
+  //The second constructor load an existing calibration
   
   //Kinect2::Kinect2Grabber<pcl::PointXYZRGB> k2g("./images/rgb/", "./images/ir/", 16, cv::Size(6,9), 0.025 );
   pcl::Kinect2Grabber::Kinect2Grabber<pcl::PointXYZRGB> k2g("./rgb_calibration.yaml", "./depth_calibration.yaml", "./pose_calibration.yaml");
