d0bccfcc738cfa6d8b833891ecca6b7605ec0d36
Giacomo Dabisias <g.dabisias@sssup.it>
2015-09-23 14:17:13 +0200

    new reworked kinect2 grabber

---
+++ k2g.h
@@ -0,0 +1,172 @@
+#include <libfreenect2/libfreenect2.hpp>
+#include <libfreenect2/frame_listener_impl.h>
+#include <libfreenect2/threading.h>
+#include <libfreenect2/packet_pipeline.h>
+#include <libfreenect2/registration.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <thread>
+#include <vector>
+#include <signal.h>
+#include <string>
+#include <iostream>
+#include <Eigen/Core>
+
+bool stop = false;
+
+enum processor{
+	CPU, OPENCL, OPENGL
+};
+
+void sigint_handler(int s)
+{
+	stop = true;
+}
+
+class K2G {
+
+public:
+
+	K2G(processor p): undistorted_(512, 424, 4), registered_(512, 424, 4), listener_(libfreenect2::Frame::Color | libfreenect2::Frame::Ir | libfreenect2::Frame::Depth){
+
+		signal(SIGINT,sigint_handler);
+
+		if(freenect2_.enumerateDevices() == 0)
+		{
+			std::cout << "no kinect2 connected!" << std::endl;
+			exit(-1);
+		}
+
+		serial_ = freenect2_.getDefaultDeviceSerialNumber();
+		switch(p){
+			case CPU:
+				std::cout << "creating CPU processor" << std::endl;
+				pipeline_ = new libfreenect2::CpuPacketPipeline();
+				break;
+			case OPENCL:
+				std::cout << "creating OpenCL processor" << std::endl;
+				pipeline_ = new libfreenect2::OpenCLPacketPipeline();
+				break;
+			case OPENGL:
+				std::cout << "creating OpenGL processor" << std::endl;
+				pipeline_ = new libfreenect2::OpenGLPacketPipeline();
+				break;
+			default:
+				std::cout << "creating CPU processor" << std::endl;
+				pipeline_ = new libfreenect2::CpuPacketPipeline();
+				break;
+		}
+		
+		dev_ = freenect2_.openDevice(serial_, pipeline_);
+		dev_->setColorFrameListener(&listener_);
+		dev_->setIrAndDepthFrameListener(&listener_);
+		dev_->start();
+
+		registration_ = new libfreenect2::Registration(dev_->getIrCameraParams(), dev_->getColorCameraParams());
+
+		d_matrix_ = Eigen::Matrix4d::Identity();
+		d_matrix_(0,0) = dev_->getIrCameraParams().fx;
+		d_matrix_(0,2) = dev_->getIrCameraParams().cx;
+		d_matrix_(1,1) = dev_->getIrCameraParams().fy;
+		d_matrix_(1,2) = dev_->getIrCameraParams().cy;
+
+		d_matrix_inv_ = d_matrix_.inverse();
+		std::cout << "Camera matrix:" << std::endl;
+		std::cout << d_matrix_ << std::endl;
+ 	}
+
+ 	/*
+	void addCallback(std::function f){
+		callbacks_.push_back(f);
+	}*/
+
+	libfreenect2::Freenect2Device::IrCameraParams getIrParameters(){
+		libfreenect2::Freenect2Device::IrCameraParams ir = dev_->getIrCameraParams();
+		return ir;
+	}
+
+	libfreenect2::Freenect2Device::ColorCameraParams getRgbParameters(){
+		libfreenect2::Freenect2Device::ColorCameraParams rgb = dev_->getColorCameraParams();
+		return rgb;
+	}
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr getCloud(){
+		
+		listener_.waitForNewFrame(frames_);
+		libfreenect2::Frame * rgb = frames_[libfreenect2::Frame::Color];
+		libfreenect2::Frame * depth = frames_[libfreenect2::Frame::Depth];
+
+		registration_->apply(rgb, depth, &undistorted_, &registered_);
+		const short w = undistorted_.width;
+		const short h = undistorted_.height;
+
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>(w, h));
+
+		const float * itD0 = (float *)undistorted_.data;
+		const char * itRGB0 = (char *)registered_.data;
+		pcl::PointXYZRGB * itP = &cloud->points[0];
+		
+		for(int y = 0; y < h; ++y)
+		{	
+			const unsigned int offset = y * w;
+			const float * itD = itD0 + offset;
+			const char * itRGB = itRGB0 + offset*4;
+			
+			for(size_t x = 0; x < w; ++x, ++itP, ++itD, itRGB += 4 )
+			{
+				const float depth_value = *itD / 1000.0f;
+				
+				if(isnan(depth_value))
+				{
+					itP->x = itP->y = itP->z = std::numeric_limits<float>::quiet_NaN();
+					itP->rgba = 0;
+
+				}else{
+
+					Eigen::Vector4d psd(x, y, 1.0, 1.0 / depth_value);
+					pworld_ = d_matrix_inv_ * psd * depth_value;
+					itP->z = depth_value;
+
+					itP->x = isnan(pworld_.x()) ? 0 : pworld_.x();
+					itP->y = isnan(pworld_.y()) ? 0 : pworld_.y();
+
+					itP->b = itRGB[0];
+					itP->g = itRGB[1];
+					itP->r = itRGB[2];
+				}
+			}
+		}
+
+		listener_.release(frames_);
+		return cloud;
+	}
+
+	void shutDown(){
+		dev_->stop();
+  		dev_->close();
+	}
+
+	cv::Mat * getColor(){
+		listener_.waitForNewFrame(frames_);
+		libfreenect2::Frame * rgb = frames_[libfreenect2::Frame::Color];
+		cv::Mat * tmp = new cv::Mat(rgb->height, rgb->width, CV_8UC4, rgb->data);
+		listener_.release(frames_);
+		return tmp;
+	}
+private:
+
+	libfreenect2::Freenect2 freenect2_;
+	libfreenect2::Freenect2Device * dev_ = 0;
+	libfreenect2::PacketPipeline * pipeline_ = 0;
+	libfreenect2::Registration * registration_ = 0;
+	libfreenect2::SyncMultiFrameListener listener_;
+	libfreenect2::FrameMap frames_;
+	libfreenect2::Frame undistorted_, registered_;
+	Eigen::Vector4d pworld_;
+	Eigen::Matrix4d d_matrix_, d_matrix_inv_;
+	//std::vector<std::function> callbacks_;
+	std::string serial_;
+};
--- test.cpp
+++ test.cpp
@@ -12,23 +12,21 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 //This is preliminary software and/or hardware and APIs are preliminary and subject to change.
-#include "Kinect2Grabber.hpp"
+#include "k2g.h"
+#include <pcl/visualization/cloud_viewer.h>
 #include <chrono>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/core/core.hpp>        
+#include <opencv2/features2d/features2d.hpp>
 
 int main(int argc, char *argv[])
 {
-  //The first constructor calibrates the cameras using images inside the specified folders.
-  //The parameters are: rgb image folder, depth image folder, number of images, size of the checkboard, size of the check board squares in m
-  //The second constructor load an existing calibration
   
-  //Kinect2Grabber::Kinect2Grabber<pcl::PointXYZRGB> k2g("./images512/rgb/", "./images512/ir/", 16, cv::Size(6,9), 0.025 );
-  Kinect2Grabber::Kinect2Grabber<pcl::PointXYZRGB> k2g("../calibration/rgb_calibration.yaml", "../calibration/depth_calibration.yaml", "../calibration/pose_calibration.yaml");
-  //Kinect2Grabber::Kinect2Grabber<pcl::PointXYZRGB> k2g;
   boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>> cloud;
-  k2g.setDistance(10000);
+  K2G k2g(OPENGL);
   std::cout << "getting cloud" << std::endl;
-  cloud = k2g.getFullCloud();
-  //cloud = k2g.getCloud();
+  cloud = k2g.getCloud();
 
   cloud->sensor_orientation_.w() = 0.0;
   cloud->sensor_orientation_.x() = 1.0;
@@ -39,18 +37,16 @@ int main(int argc, char *argv[])
   pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
   viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb, "sample cloud");
   viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
-  while (!viewer->wasStopped ()) {
+  
+  while (!viewer->wasStopped()) {
     viewer->spinOnce ();
     using namespace std::chrono;
     static high_resolution_clock::time_point last;
 
-    auto tnow = high_resolution_clock::now();
-    cloud = k2g.getFullCloud();
-
-    //cloud = k2g.getCloud();
+    auto tnow = high_resolution_clock::now();   
+    cloud = k2g.getCloud();
     auto tpost = high_resolution_clock::now();
-    std::cout << "delta " << duration_cast<duration<double>>(tpost-last).count()*1000 << " " << duration_cast<duration<double>>(tpost-tnow).count()*1000 << std::endl;
-    last = tpost;
+    std::cout << "delta " << duration_cast<duration<double>>(tpost-tnow).count()*1000 << std::endl;
     
     pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
     viewer->updatePointCloud<pcl::PointXYZRGB> (cloud, rgb, "sample cloud"); 
