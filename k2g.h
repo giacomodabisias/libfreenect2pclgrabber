8ba2d85bf27e9885cd109ba4fc3b6ecb95bdbf4b
Giacomo Dabisias <g.dabisias@sssup.it>
2015-09-25 11:03:48 +0200

    add license

--- k2g.h
+++ k2g.h
@@ -1,3 +1,24 @@
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+@Author 
+Giacomo. Dabisias, PhD Student
+PERCRO, (Laboratory of Perceptual Robotics)
+Scuola Superiore Sant'Anna
+via Luigi Alamanni 13D, San Giuliano Terme 56010 (PI), Italy
+*/
+
 #include <libfreenect2/libfreenect2.hpp>
 #include <libfreenect2/frame_listener_impl.h>
 #include <libfreenect2/threading.h>
