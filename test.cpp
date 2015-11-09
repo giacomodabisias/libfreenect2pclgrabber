d7debc9032b83c909f5a1c4961d533c770f79c88
Andrew Hundt <ATHundt@gmail.com>
2015-11-08 23:57:58 -0500

    added command line option to select processor and output a PLY file for one frame.

--- test.cpp
+++ test.cpp
@@ -20,11 +20,48 @@ via Luigi Alamanni 13D, San Giuliano Terme 56010 (PI), Italy
 #include <pcl/visualization/cloud_viewer.h>
 #include <chrono>
 
+// extra headers for writing out ply file
+#include <pcl/console/print.h>
+#include <pcl/console/parse.h>
+#include <pcl/console/time.h>
+#include <pcl/conversions.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/PCLPointCloud2.h>
+
+using namespace pcl;
+using namespace pcl::io;
+using namespace pcl::console;
+
+template<typename Cloud>
+void
+saveCloud (const std::string &filename, const Cloud &cloud, bool binary, bool use_camera)
+{
+  TicToc tt;
+  tt.tic ();
+
+  print_highlight ("Saving "); print_value ("%s ", filename.c_str ());
+  
+  pcl::PLYWriter writer;
+  writer.write (filename, cloud, Eigen::Vector4f::Zero (), Eigen::Quaternionf::Identity (), binary, use_camera);
+  
+  print_info ("[done, "); print_value ("%g", tt.toc ()); print_info (" ms : "); print_value ("%d", cloud.width * cloud.height); print_info (" points]\n");
+}
+
 int main(int argc, char *argv[])
 {
-  
+  std::cout << "Syntax is: " << argv[0] << " [-processor 0|1|2] [output.ply]\n -processor options 0,1,2 correspond to CPU, OPENCL, and OPENGL respectively\n";
+  processor freenectprocessor = OPENGL;
+  std::vector<int> ply_file_indices;
+  if(argc>1){
+      int fnpInt;
+      ply_file_indices = parse_file_extension_argument (argc, argv, ".ply");
+      parse_argument (argc, argv, "-processor", fnpInt);
+      freenectprocessor = static_cast<processor>(fnpInt);
+      
+  }
+    
   boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>> cloud;
-  K2G k2g(OPENGL);
+  K2G k2g(freenectprocessor);
   std::cout << "getting cloud" << std::endl;
   cloud = k2g.getCloud();
 
@@ -38,7 +75,8 @@ int main(int argc, char *argv[])
   viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb, "sample cloud");
   viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
   
-  while (!viewer->wasStopped()) {
+  bool done = false;
+  while ((!viewer->wasStopped()) && (!done)) {
     viewer->spinOnce ();
     using namespace std::chrono;
     static high_resolution_clock::time_point last;
@@ -49,6 +87,13 @@ int main(int argc, char *argv[])
     std::cout << "delta " << duration_cast<duration<double>>(tpost-tnow).count()*1000 << std::endl;
     pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
     viewer->updatePointCloud<pcl::PointXYZRGB> (cloud, rgb, "sample cloud"); 
+    
+    if(ply_file_indices.size() > 0 ){
+        pcl::PCLPointCloud2 cloud2;
+        pcl::toPCLPointCloud2(*cloud,cloud2);
+        saveCloud(std::string(argv[ply_file_indices[0]]),cloud2,false,false);
+        done = true;
+    }
   }
 
   k2g.shutDown();
