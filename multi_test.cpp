bb1d61a1f2d44464bd2e2893aad12bb81fe476c1
Giacomo Dabisias <g.dabisias@sssup.it>
2016-03-18 16:02:22 +0100

    first test with vectors

--- multi_test.cpp
+++ multi_test.cpp
@@ -26,19 +26,25 @@ via Luigi Alamanni 13D, San Giuliano Terme 56010 (PI), Italy
 #include <pcl/console/time.h>
 #include <pcl/io/ply_io.h>
 
+static int n_ = -1;
+
+struct Counter{
+	Counter()
+	{
+		n_++;
+	}
+	static void reset(){n_ = -1;}
+};
 
 struct PlySaver{
 
-  PlySaver(boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>> cloud_1, boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>> cloud_2, bool binary, bool use_camera, K2G & k2g_1, K2G & k2g_2): 
-           cloud_1_(cloud_1), cloud_2_(cloud_2), binary_(binary), use_camera_(use_camera), k2g_1_(k2g_1), k2g_2_(k2g_2){}
+  PlySaver(std::vector<boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>>> & clouds, bool binary, bool use_camera, std::vector<K2G> & kinects): 
+           binary_(binary), use_camera_(use_camera), clouds_(clouds), kinects_(kinects){}
 
-  boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>> cloud_1_;
-  boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>> cloud_2_;
-  K2G & k2g_1_;
-  K2G & k2g_2_;
+  std::vector<boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>>> & clouds_;
+  std::vector<K2G> & kinects_;
   bool binary_;
   bool use_camera_;
-
 };
 
 
@@ -56,12 +62,17 @@ KeyboardEventOccurred(const pcl::visualization::KeyboardEvent &event, void * dat
       pcl::PLYWriter writer;
       std::chrono::high_resolution_clock::time_point p = std::chrono::high_resolution_clock::now();
       std::string now = std::to_string((long)std::chrono::duration_cast<std::chrono::milliseconds>(p.time_since_epoch()).count());
-      writer.write ("cloud_1_" + now + ".ply", *(s->cloud_1_), s->binary_, s->use_camera_);
-      writer.write ("cloud_2_" + now + ".ply", *(s->cloud_2_), s->binary_, s->use_camera_);
-      cv::imwrite("color_1_" + now + ".jpg", s->k2g_1_.getColor());
-      cv::imwrite("color_2_" + now + ".jpg", s->k2g_2_.getColor());
-      
+      for(size_t i = 0; i < s->kinects_.size(); ++i){
+      	writer.write ("cloud_"+ std::to_string(i) + "_" + now + ".ply", *(s->clouds_[i]), s->binary_, s->use_camera_);
+      	cv::imwrite("color_" + std::to_string(i) + "_" + now + ".jpg", s->kinects_[i].getColor());
+      }
       std::cout << "saved " << "cloud and color " + now << std::endl;
+    }
+    if(pressed == "x")
+    {
+        for(auto & k : s->kinects_)
+        	k.storeParameters();
+        std::cout << "stored calibration parameters" << std::endl;
     }
   }
 }
@@ -69,70 +80,69 @@ KeyboardEventOccurred(const pcl::visualization::KeyboardEvent &event, void * dat
 int main(int argc, char *argv[])
 {
   std::cout << "Syntax is: " << argv[0] << " [-processor 0|1|2] -processor options 0,1,2,3 correspond to CPU, OPENCL, OPENGL, CUDA respectively" << std::endl;
-  std::cout << "followed by the two kinect serials" << std::endl;
+  std::cout << "followed by the kinect2 serials and a last 1 for mirroring" << std::endl;
+  std::cout << "Press \'s\' to store both clouds and color images." << std::endl;
+  std::cout << "Press \'x\' to store both calibrations." << std::endl;
 
   processor freenectprocessor = OPENGL;
   std::vector<int> ply_file_indices;
-  if(argc < 4){
-    std::cout << "Syntax is: " << argv[0] << " [-processor 0|1|2] -processor options 0,1,2,3 correspond to CPU, OPENCL, OPENGL, CUDA respectively" << std::endl;
-    std::cout << "followed by the two kinect serials" << std::endl;
+  if(argc < 3){
+    std::cout << "Wrong syntax! specify at least processor and one serial" << std::endl;
     return -1;
   }
 
   freenectprocessor = static_cast<processor>(atoi(argv[1]));
-  
-  boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>> cloud_1;
-  boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>> cloud_2;
-  K2G k2g_1(freenectprocessor, argc == 5 ? true : false, argv[2]); //"005157353647"
-  K2G k2g_2(freenectprocessor, argc == 5 ? true : false, argv[3]); //"500320441942"
+  bool mirroring = atoi(argv[argc-1]) == 1 ? true : false;
 
-  std::cout << "getting cloud" << std::endl;
-  cloud_1 = k2g_1.getCloud();
-  cloud_2 = k2g_2.getCloud();
+  if(mirroring)
+  	std::cout << "mirroring enabled" << std::endl;
 
-  cloud_1->sensor_orientation_.w() = 0.0;
-  cloud_1->sensor_orientation_.x() = 1.0;
-  cloud_1->sensor_orientation_.y() = 0.0;
-  cloud_1->sensor_orientation_.z() = 0.0;
+  int kinect2_count = mirroring ? argc - 3 : argc - 2;
+  std::cout << "loading " << kinect2_count << " devices" << std::endl;
+  std::cout << n_ << std::endl;
+  std::vector<K2G> kinects(kinect2_count, (Counter(), K2G(freenectprocessor, mirroring, argv[n_ + 2])));
+  Counter::reset();
+  std::cout << "reset to " << n_ << std::endl;
+  std::vector<boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>>> clouds(kinect2_count, (Counter(), kinects[ n_].getCloud()));
+  std::cout << n_ << std::endl;
 
-  cloud_2->sensor_orientation_.w() = 0.0;
-  cloud_2->sensor_orientation_.x() = 1.0;
-  cloud_2->sensor_orientation_.y() = 0.0;
-  cloud_2->sensor_orientation_.z() = 0.0;
 
-  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer_1(new pcl::visualization::PCLVisualizer ("3D Viewer_1"));
-  viewer_1->setBackgroundColor (0, 0, 0);
-  pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb_1(cloud_1);
-  viewer_1->addPointCloud<pcl::PointXYZRGB>(cloud_1, rgb_1, "sample cloud_1");
-  viewer_1->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud_1");
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(new pcl::visualization::PCLVisualizer ("3D viewer"));
+  viewer->setBackgroundColor (0, 0, 0);
 
-  pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb_2(cloud_2);
-  viewer_1->addPointCloud<pcl::PointXYZRGB>(cloud_2, rgb_2, "sample cloud_2");
-  viewer_1->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud_2");
+  for(size_t i = 0; i < kinect2_count; ++i)
+  {
+  	clouds[i]->sensor_orientation_.w() = 0.0;
+  	clouds[i]->sensor_orientation_.x() = 1.0;
+  	clouds[i]->sensor_orientation_.y() = 0.0;
+  	clouds[i]->sensor_orientation_.z() = 0.0; 
 
-  PlySaver ps_1(cloud_1, cloud_2, false, false, k2g_1, k2g_2);
-  viewer_1->registerKeyboardCallback(KeyboardEventOccurred, (void*)&ps_1);
+  	viewer->addPointCloud<pcl::PointXYZRGB>(clouds[i], pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB>(clouds[i]), "sample cloud_" + i);
+  	viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud_" + i);
+  }
 
-  while(!viewer_1->wasStopped()){
+  PlySaver ps(clouds, false, false, kinects);
+  viewer->registerKeyboardCallback(KeyboardEventOccurred, (void*)&ps);
+  std::cout << "starting cycle" << std::endl;
+  while(!viewer->wasStopped()){
 
-    viewer_1->spinOnce ();
+    viewer->spinOnce ();
 
     std::chrono::high_resolution_clock::time_point tnow = std::chrono::high_resolution_clock::now();
 
-    cloud_1 = k2g_1.updateCloud(cloud_1);
-    cloud_2 = k2g_2.updateCloud(cloud_2);
+    for(size_t i = 0; i < kinect2_count; ++i)
+    	clouds[i] = kinects[i].updateCloud(clouds[i]);
 
     std::chrono::high_resolution_clock::time_point tpost = std::chrono::high_resolution_clock::now();
     std::cout << "delta " << std::chrono::duration_cast<std::chrono::duration<double>>(tpost-tnow).count() * 1000 << std::endl;
-    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb_1(cloud_1);
-    viewer_1->updatePointCloud<pcl::PointXYZRGB> (cloud_1, rgb_1, "sample cloud_1");
-    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb_2(cloud_2);
-    viewer_1->updatePointCloud<pcl::PointXYZRGB> (cloud_2, rgb_2, "sample cloud_2");    	
 
+    for(size_t i = 0; i < kinect2_count; ++i)
+    	viewer->updatePointCloud<pcl::PointXYZRGB> (clouds[i], pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> (clouds[i]), "sample cloud_" + i);
   }
 
-  k2g_1.shutDown();
-  k2g_2.shutDown();
+  for(auto & k : kinects)
+  	k.shutDown();
+
   return 0;
 }
 
